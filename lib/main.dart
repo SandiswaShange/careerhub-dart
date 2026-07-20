73fd22b200341431fb9c54d38efa851db6cf2f04
SandiswaShange <sandiswashange32@gmail.com>
2026-07-21 01:41:36 +0200

    feat: Core provider stubs and async startup

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,32 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'router/app_router.dart';
+import 'package:isar/isar.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-void main() {
-  runApp(const ProviderScope(child: CareerHubApp()));
+import 'core/isar_provider.dart';
+import 'core/prefs_provider.dart';
+import 'data/job_cache.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  final directory = await getApplicationDocumentsDirectory();
+
+  final isar = await Isar.open(
+    [JobCacheSchema,],
+    directory: directory.path,
+  );
+  final prefs = await SharedPreferences.getInstance();
+
+  runApp(
+    ProviderScope(
+      overrides: [
+        isarProvider.overrideWithValue(isar),
+        prefsProvider.overrideWithValue(prefs),
+      ],
+      child: const CareerHubApp(),
+    ),
+  );
 }
 
 class CareerHubApp extends StatelessWidget {
